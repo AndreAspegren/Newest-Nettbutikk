724d199b7386ea1dd067af59f3536f3456a2ed12
AndreAspegren <157694115+AndreAspegren@users.noreply.github.com>
2024-06-11 09:25:14 +0200

    x

--- wwwroot/Model.js
+++ wwwroot/Model.js
@@ -16,81 +16,10 @@ let model = {
         },
 
         
-        },
-
-
+    },
 
     data: {
-
-        products: 
-            [
-                {
-                    "id": 0,
-                    "name": "Usb kabel 1",
-                    "description": "fin usb kabel",
-                    "price": 100,
-                    "sale": 0,
-                    "pictureURL": "kek",
-                    "numberInInventory": 100,
-                    "technicalInfo": "Fin 1 meter lang usb kabel",
-                    "category": "Cable"
-                },
-                {
-                    "id": 1,
-                    "name": "USB kabel 2",
-                    "description": "finere usb kabel",
-                    "price": 120,
-                    "sale": 0,
-                    "pictureURL": "woah",
-                    "numberInInventory": 120,
-                    "technicalInfo": "Finere 2 meter lang usb kabel",
-                    "category": "Cable"
-                },
-                {
-                    "id": 2,
-                    "name": "USB kabel 3 ",
-                    "description": "fineste usb kabel",
-                    "price": 140,
-                    "sale": 0,
-                    "pictureURL": "hiya",
-                    "numberInInventory": 140,
-                    "technicalInfo": "Fineste 3 meter lang usb kabel",
-                    "category": "Cable"
-                },
-                {
-                    "id": 3,
-                    "name": "Luftfukter 1",
-                    "description": "fin luftfukter",
-                    "price": 500,
-                    "sale": 0,
-                    "pictureURL": "lmao",
-                    "numberInInventory": 10,
-                    "technicalInfo": "Fin liten luftfukter",
-                    "category": "Humidifier"
-                },
-                {
-                    "id": 4,
-                    "name": "Luftfukter 2",
-                    "description": "finere luftfukter",
-                    "price": 700,
-                    "sale": 0,
-                    "pictureURL": "dopadown",
-                    "numberInInventory": 12,
-                    "technicalInfo": "Finere medium luftfukter",
-                    "category": "Humidifier"
-                },
-                {
-                    "id": 5,
-                    "name": "Luftfukter 3",
-                    "description": "fineste luftfukter",
-                    "price": 900,
-                    "sale": 0,
-                    "pictureURL": "whatitdoesdo",
-                    "numberInInventory": 14,
-                    "technicalInfo": "Fineste stor luftfukter",
-                    "category": "Humidifier"
-                }
-            ]
+        products: await axios.get(`https://localhost:8000/textobject`),
     }
 
 
