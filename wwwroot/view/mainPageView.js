bd4d8a23455e6713a80854c92ae02b0702803c6b
RuneSmedhaugen <157691381+RuneSmedhaugen@users.noreply.github.com>
2024-06-16 11:37:56 +0200

    test

--- wwwroot/view/mainPageView.js
+++ wwwroot/view/mainPageView.js
@@ -9,6 +9,22 @@ function homescreen() {
         <div id="login">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKIXaiO9wCCeAHXEwioJntszdoi9YiEIDDgA&s" id="logo" onclick="logo" placeholder="Logo"/>
         <button id="login"> Logg inn </button>
+        <div id="registerdiv">
+        <button id="register" onclick="register()"> Registrer bruker </button>
+        <div id="registerinputs" style="display: none;">
+        <input onchange="model.input.register.FirstName = this.value" placeholder = "Fornavn"/>
+        <input onchange="model.input.register.LastName = this.value" placeholder = "Etternavn"/>
+        <input onchange="model.input.register.Email = this.value" placeholder = "E-post"/>
+        <input onchange="model.input.register.PhoneNumber = this.value" placeholder = "Telefonnummer"/>
+        <input onchange="model.input.register.Password = this.value" placeholder = "Passord"/>
+      
+        <input onchange="model.input.register.Address = this.value" placeholder = "Addresse"/>
+        <input onchange="model.input.register.ZipCode = this.value" placeholder = "Postnummer"/>
+        <input onchange="model.input.register.>ZipArea = this.value" placeholder = "Poststed"/>
+        <button onclick="genUser">Registrer bruker</button>
+        </div>
+
+        </div>
         <button id="cart"> Handlekurv </button>
         <button id="huskerikkehvadensistevar">husker ikke hva denne gjør</button>
         </div>
@@ -23,16 +39,7 @@ function homescreen() {
         <div id="aboutus">Vi er best, kjøp fra oss</div>
 
         
-        <input onchange="model.input.register.FirstName = this.value" placeholder = "Fornavn"/>
-        <input onchange="model.input.register.LastName = this.value" placeholder = "Etternavn"/>
-        <input onchange="model.input.register.Email = this.value" placeholder = "E-post"/>
-        <input onchange="model.input.register.PhoneNumber = this.value" placeholder = "Telefonnummer"/>
-        <input onchange="model.input.register.Password = this.value" placeholder = "Passord"/>
-      
-        <input onchange="model.input.register.Address = this.value" placeholder = "Addresse"/>
-        <input onchange="model.input.register.ZipCode = this.value" placeholder = "Postnummer"/>
-        <input onchange="model.input.register.>ZipArea = this.value" placeholder = "Poststed"/>
-        <button onclick="genUser">Registrer bruker</button>
+
 
 
 
@@ -45,27 +52,3 @@ function updateview(newView)
     newView ? (model.app.currentView = newView, window[newView]()) : window[model.app.currentView]()
 }
 
-async function genUser() {
-    if (await badInputs()) return
-    let user = {
-        FirstName: model.input.register.FirstName,
-        LastName: model.input.register.LastName,
-        Email: model.input.register.Email,
-        PhoneNumber: model.input.register.PhoneNumber,
-        Password: model.input.register.Password,
-        Address: model.input.register.Address,
-        ZipCode: model.input.register.ZipCode,
-        ZipArea: model.input.register.ZipArea,
-    }
-
-    model.input.register.forEach(i => i = "")
-    let query = await axios.Post("/genUser", user)
-}
-
-async function badInputs() {
-    let badInputMsg = ""
-    model.input.register.forEach((i, value) => {
-        if (!i) badInputMsg += ``;
-    })
-    return false
-}
